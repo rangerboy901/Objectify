da02d32a80146d0c4ab489cede4d417dd47c27dd
rangerboy574 <jdeweese574@gmail.com>
2023-02-17 09:17:03 -0600

    Initial Commit

---
+++ Objectify/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Objectify
+//
+//  Created by Joseph  DeWeese on 2/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Objectify/ObjectifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectifyApp.swift
+//  Objectify
+//
+//  Created by Joseph  DeWeese on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ObjectifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
